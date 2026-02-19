7d78eb700eb9b3438fed351bf987cec0f94de04c
jhe56 <jhe56@dons.usfca.edu>
2026-02-18 20:17:13 -0800

    testing pandas read csv datafram

--- populator.py
+++ populator.py
@@ -1,1 +1,7 @@
 # Partnered with Ryan from 362 to follow along an outline of the project recommended by chatgpt
+from person import person
+from pandas import read_csv
+
+df = read_csv('first_names.csv')
+
+print(df['name'])
