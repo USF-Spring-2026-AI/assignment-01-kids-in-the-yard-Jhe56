63297e9141e9a8fc50afd2697273385a24f9cfb3
jhe56 <jhe56@dons.usfca.edu>
2026-02-18 20:52:04 -0800

    removed whitespae

--- person_factory.py
+++ person_factory.py
@@ -4,9 +4,7 @@ from pandas import read_csv
 
 #gpt recommends person factory
 class Person_factory:
-
     def __init__(self):
-    
         #print(fn['name'][0]) -- for legal reference
         self.fn = read_csv('first_names.csv')
         self.ln = read_csv('last_names.csv')
